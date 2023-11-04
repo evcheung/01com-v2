966caf95a45fa6c0a1dfa6f383d85b54adfb0159
Evelyn <ev.cheung33@gmail.com>
2023-11-04 11:53:56 +0100

    get studio deploy working

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,7 +4,14 @@
 **/
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+// const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const projectId = "7o47py1b"
+// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const dataset = 'production'
 
-export default defineCliConfig({ api: { projectId, dataset } })
+export default defineCliConfig({
+  api: { projectId, dataset },
+  project: {
+    basePath: '/studio'
+  },
+})
--- sanity/env.ts
+++ sanity/env.ts
@@ -1,15 +1,17 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-09-29'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset = 'production'
+// assertValue(
+//   process.env.NEXT_PUBLIC_SANITY_DATASET,
+//   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+// )
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+export const projectId = "7o47py1b"
+//  assertValue(
+//   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+//   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+// )
 
 export const useCdn = false
 

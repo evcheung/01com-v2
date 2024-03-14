36c8f7b9056d3ea8bbc79f0a8d3c38a7bce368c1
Evelyn <ev.cheung33@gmail.com>
2024-03-14 01:15:29 -0400

    lint

--- components/im-in-touch/IITFooterNav.tsx
+++ components/im-in-touch/IITFooterNav.tsx
@@ -2,10 +2,10 @@ import { useWindowSize } from 'usehooks-ts'
 import { IITFooterNavMobile } from './IITFooterNavMobile'
 import { IITFooterNavDesktop } from './IITFooterNavDesktop'
 
-export const IITFooterNav = ({ subSite, width }: { subSite?: 'go' | 'gomail' | 'server' | 'securekey', width: number }) => {
+export const IITFooterNav = ({ width }: { width: number }) => {
   return (
     <>
-      {width > 760 ? <IITFooterNavDesktop subSite={subSite} /> : <IITFooterNavMobile subSite={subSite} />}
+      {width > 760 ? <IITFooterNavDesktop /> : <IITFooterNavMobile />}
     </>
   )
 }

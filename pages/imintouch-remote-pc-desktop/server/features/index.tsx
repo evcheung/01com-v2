bf2aff22f627cf4f2baf66acdd62ae7711e16db2
evcheung <ev.cheung33@gmail.com>
2025-09-06 00:22:18 -0400

    copy change

--- pages/imintouch-remote-pc-desktop/server/features/index.tsx
+++ pages/imintouch-remote-pc-desktop/server/features/index.tsx
@@ -1,17 +1,25 @@
 import Head from "next/head";
 import styled from "styled-components";
-import { Box, Text, TextColors, TextVariants } from "../../../../components/core";
+import {
+  Box,
+  Text,
+  TextColors,
+  TextVariants
+} from "../../../../components/core";
 import { breakpoints } from "../../../../utils/breakpoints";
 import { IITPageBanner } from "../../../../components/im-in-touch/IITPageBanner";
 import IITLayout from "../../../../components/im-in-touch/IITLayout";
 import { theme } from "../../../../theme";
 import Image from "next/image";
 import Link from "next/link";
-import { FullPageContentContainer, PageContentContainer } from "../../../../components/PageContentContainer";
+import {
+  FullPageContentContainer,
+  PageContentContainer
+} from "../../../../components/PageContentContainer";
 import { FeatureCard } from "../../../../components/im-in-touch/IITFeatureCard";
-import Settings from "../../../../public/assets/settings.png"
-import List from "../../../../public/assets/list.png"
-import LinkArrow from "../../../../public/assets/link-arrow.png"
+import Settings from "../../../../public/assets/settings.png";
+import List from "../../../../public/assets/list.png";
+import LinkArrow from "../../../../public/assets/link-arrow.png";
 import { useWindowSize } from "usehooks-ts";
 
 const FeaturesGrid = styled(Box)`
@@ -22,13 +30,13 @@ const FeaturesGrid = styled(Box)`
   ${breakpoints("grid-template-columns", "", [{ 680: "1fr" }])}
 `;
 
-const FeatureRowItemContainer = styled(Box) <{
+const FeatureRowItemContainer = styled(Box)<{
   index: number;
   borderSecondItem?: boolean;
 }>`
   padding: 16px 0;
   border-bottom: 1px solid ${theme.colors.neutral.md};
-  border-top: ${(props) =>
+  border-top: ${props =>
     props.index === 0 || (props.index === 1 && props.borderSecondItem)
       ? `1px solid ${theme.colors.neutral.md}`
       : "none"};
@@ -52,7 +60,7 @@ const FeatureRowItem = ({
   index,
   pageRoute,
   description,
-  borderSecondItem,
+  borderSecondItem
 }: {
   index: number;
   pageRoute?: string;
@@ -83,99 +91,106 @@ const FeatureRowItem = ({
 
 const ADMINISTRATION_FEATURES = [
   {
-    description: "Add/Remove/Edit user accounts of your I'm InTouch remote access system",
-    pageRoute: "/imintouch-remote-pc-desktop/server/features/add-remove-accounts",
+    description:
+      "Add/Remove/Edit user accounts of your I'm InTouch remote access system",
+    pageRoute:
+      "/imintouch-remote-pc-desktop/server/features/add-remove-accounts"
   },
   {
-    description: "Manage Access Rights of user accounts including time-of-accessibility",
-    pageRoute: "/imintouch-remote-pc-desktop/server/features/access-rights",
+    description:
+      "Manage Access Rights of user accounts including time-of-accessibility",
+    pageRoute: "/imintouch-remote-pc-desktop/server/features/access-rights"
   },
   {
     description: "Create Template for easy bulk management of user accounts",
-    pageRoute: "/imintouch-remote-pc-desktop/server/features/access-rights",
+    pageRoute: "/imintouch-remote-pc-desktop/server/features/access-rights"
   },
   {
     description: "Monitor Usage Statistics of user accounts",
-    pageRoute: "/imintouch-remote-pc-desktop/server/features/statistics",
-  },
+    pageRoute: "/imintouch-remote-pc-desktop/server/features/statistics"
+  }
 ];
 
 const GENERAL_FEATURES = [
   {
-    description: "Controlling desktop computer as if you were sitting in front of it",
+    description:
+      "Controlling desktop computer as if you were sitting in front of it"
   },
   {
-    description: "Wireless notification of new emails",
+    description: "Wireless notification of new emails"
   },
   {
-    description: "Remote printing",
+    description: "Remote printing"
   },
   {
-    description: "Online calendar",
+    description: "Online calendar"
   },
   {
-    description: "Remote audio",
+    description: "Remote audio"
   },
   {
-    description: "Remote contact management",
+    description: "Remote contact management"
   },
   {
-    description: "File transfer",
+    description: "File transfer"
   },
   {
-    description: "Live streaming of webcam",
+    description: "Live streaming of webcam"
   },
   {
-    description: "Multi-Monitor support",
+    description: "Multi-Monitor support"
   },
   {
-    description: "Auto-Login/Physical Authentication via the I'm InTouch SecureKEY (optional device)",
+    description:
+      "Auto-Login/Physical Authentication via the I'm InTouch SecureKEY (optional device)"
   },
   {
-    description: "Email access",
+    description: "Email access"
   },
   {
-    description: "Guest invitation",
-  },
+    description: "Guest invitation"
+  }
 ];
 
 const GENERAL_FEATURES_MOBILE = [
   {
-    description: "Controlling desktop computer as if you were sitting in front of it",
+    description:
+      "Controlling desktop computer as if you were sitting in front of it"
   },
   {
-    description: "Remote printing",
+    description: "Remote printing"
   },
   {
-    description: "Remote audio",
+    description: "Remote audio"
   },
   {
-    description: "File transfer",
+    description: "File transfer"
   },
   {
-    description: "Multi-Monitor support",
+    description: "Multi-Monitor support"
   },
   {
-    description: "Email access",
+    description: "Email access"
   },
   {
-    description: "Wireless notification of new emails",
+    description: "Wireless notification of new emails"
   },
   {
-    description: "Online calendar",
+    description: "Online calendar"
   },
   {
-    description: "Remote contact management",
+    description: "Remote contact management"
   },
   {
-    description: "Live streaming of webcam",
+    description: "Live streaming of webcam"
   },
   {
-    description: "Auto-Login/Physical Authentication via the I'm InTouch SecureKEY (optional device)",
+    description:
+      "Auto-Login/Physical Authentication via the I'm InTouch SecureKEY (optional device)"
   },
   {
-    description: "Guest invitation",
-  },
+    description: "Guest invitation"
+  }
 ];
 
 const StyledLink = styled(Link)`
@@ -183,7 +198,7 @@ const StyledLink = styled(Link)`
 `;
 
 export default function ServerFeaturesHome() {
-  const { width } = useWindowSize()
+  const { width } = useWindowSize();
   return (
     <IITLayout subSite="server">
       <Head>
@@ -203,7 +218,7 @@ export default function ServerFeaturesHome() {
           your employees. In other words, your employees are remotely accessing
           their office computers via your own I'm InTouch servers located at
           your own office or data center instead of the servers hosted by 01
-          Communique.
+          Quantum.
         </Text>
       </IITPageBanner>
       <FullPageContentContainer>
@@ -226,25 +241,42 @@ export default function ServerFeaturesHome() {
             title="General Features"
             imgSrc={List.src}
             columns={2}
-            subText={<Text style={{ marginBottom: '24px', fontSize: '18px', lineHeight: '24px' }}>Click to <StyledLink href="/imintouch-remote-pc-desktop/features/" target="_blank">Learn More</StyledLink> from www.imintouch.com</Text>}
+            subText={
+              <Text
+                style={{
+                  marginBottom: "24px",
+                  fontSize: "18px",
+                  lineHeight: "24px"
+                }}
+              >
+                Click to{" "}
+                <StyledLink
+                  href="/imintouch-remote-pc-desktop/features/"
+                  target="_blank"
+                >
+                  Learn More
+                </StyledLink>{" "}
+                from www.imintouch.com
+              </Text>
+            }
           >
             {width > 1200
               ? GENERAL_FEATURES.map((feature, index) => (
-                <FeatureRowItem
-                  index={index}
-                  borderSecondItem={true}
-                  key={feature.description}
-                  description={feature.description}
-                />
-              ))
+                  <FeatureRowItem
+                    index={index}
+                    borderSecondItem={true}
+                    key={feature.description}
+                    description={feature.description}
+                  />
+                ))
               : GENERAL_FEATURES_MOBILE.map((feature, index) => (
-                <FeatureRowItem
-                  index={index}
-                  borderSecondItem={false}
-                  key={feature.description}
-                  description={feature.description}
-                />
-              ))}
+                  <FeatureRowItem
+                    index={index}
+                    borderSecondItem={false}
+                    key={feature.description}
+                    description={feature.description}
+                  />
+                ))}
           </FeatureCard>
         </FeaturesGrid>
       </FullPageContentContainer>

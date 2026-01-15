a2c36c3d1d64f7ecb2221ec0298b30485657ab43
seanhuangg <sean.huang@torontomu.ca>
2026-01-15 00:00:02 -0500

    fixed type issue in blockContent

--- sanity/schemas/blockContent.ts
+++ sanity/schemas/blockContent.ts
@@ -1,52 +1,45 @@
 import { defineType, defineArrayMember } from "sanity";
 
-/**
- * This is the schema type for block content used in the post document type
- * Importing this type into the studio configuration's `schema` property
- * lets you reuse it in other document types with:
- *  {
- *    name: 'someName',
- *    title: 'Some title',
- *    type: 'blockContent'
- *  }
- */
-
 export default defineType({
-  title: "Block Content",
   name: "blockContent",
+  title: "Block Content",
   type: "array",
   of: [
     defineArrayMember({
-  title: 'Block',
-  type: 'block',
-  options: {
-    styles: [
-      { title: 'Normal', value: 'normal' },
-      { title: 'H1', value: 'h1' },
-      { title: 'H2', value: 'h2' },
-      { title: 'H3', value: 'h3' },
-      { title: 'H4', value: 'h4' },
-      { title: 'Quote', value: 'blockquote' },
-    ],
-    lists: [{ title: 'Bullet', value: 'bullet' }],
-    marks: {
-      decorators: [
-        { title: 'Strong', value: 'strong' },
-        { title: 'Emphasis', value: 'em' },
+      type: "block",
+      styles: [
+        { title: "Normal", value: "normal" },
+        { title: "H1", value: "h1" },
+        { title: "H2", value: "h2" },
+        { title: "H3", value: "h3" },
+        { title: "Quote", value: "blockquote" },
       ],
-      annotations: [
-        {
-          title: 'URL',
-          name: 'link',
-          type: 'object',
-          fields: [
-            { title: 'URL', name: 'href', type: 'url' },
-          ],
-        },
+      lists: [
+        { title: "Bullet", value: "bullet" },
+        { title: "Numbered", value: "number" },
       ],
-    },
-  },
-}),
+      marks: {
+        decorators: [
+          { title: "Strong", value: "strong" },
+          { title: "Emphasis", value: "em" },
+          { title: "Code", value: "code" },
+        ],
+        annotations: [
+          defineArrayMember({
+            name: "link",
+            type: "object",
+            title: "Link",
+            fields: [
+              {
+                name: "href",
+                type: "url",
+                title: "URL",
+              },
+            ],
+          }),
+        ],
+      },
+    }),
 
 
     // You can add additional types here. Note that you can't use

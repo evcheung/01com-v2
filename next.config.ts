67ec94037795fdc6a0bdab81ca1eb088ffc0ceed
seanhuangg <sean.huang@torontomu.ca>
2026-09-10 19:45:12 -0400

    fix: configure SVG components for webpack builds

--- next.config.ts
+++ next.config.ts
@@ -118,6 +118,34 @@ const nextConfig: NextConfig = {
     ],
   },
 
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: { test?: { test?: (value: string) => boolean } }) =>
+      rule.test?.test?.(".svg"),
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule?.issuer,
+        resourceQuery: {
+          not: [...(fileLoaderRule?.resourceQuery?.not ?? []), /url/],
+        },
+        use: ["@svgr/webpack"],
+      },
+    );
+
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    return config;
+  },
+
   turbopack: {
     rules: {
       "*.svg": {
